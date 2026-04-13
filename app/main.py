4158713065cff220c26319297d5349ec64865af7
Harish marimuthu <harishmarimuthu1234@gmail.com>
2026-04-13 14:24:50 +0530

    update the backend for port

--- app/main.py
+++ app/main.py
@@ -39,4 +39,6 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
